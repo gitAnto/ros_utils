a033c2590a4c3e7844e95aad2b7775e336feeca4
Antonio Coratelli <antoniocoratelli@gmail.com>
2016-08-30 17:43:19 +0200

    Refactor

--- src/libnoise.hpp
+++ src/libnoise.hpp
@@ -1,90 +1,96 @@
 #include <boost/random.hpp>
 #include <boost/random/normal_distribution.hpp>
 
-namespace libnoise{ 
+namespace libnoise
+{
+    struct Pose
+    {
+        double x;
+        double y;
+        double z;
 
-  struct Pose {
-    double x;
-    double y;
-    double z;
-
-    double qx;
-    double qy;
-    double qz;
-    double qw;
-  };
-
-
-  class AbstractNoise { // abstract class
-  public:
-    virtual Pose run (Pose p) = 0;
-    virtual bool setParam(std::string key, double value); 
-  };
-   
-  class GaussianNoise:public AbstractNoise{
-  private:
-    double mean = 0;
-    double std_dev = 1;  
-    
-    boost::mt19937 rng; // I don't seed it on purpouse (it's not relevant)
-
-    boost::normal_distribution<> *nd;
-
-    boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > *var_nor(rng, *nd);
+        double qx;
+        double qy;
+        double qz;
+        double qw;
+    };
 
 
-  public:
-
-    GaussianNoise(){
-       nd = new boost::normal_distribution<> (mean,std_dev);   
-       var_nor = new boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > (rng, *nd);
-    }
-    
-    bool setParam(std::string key, double value){
-
-      if (key == 'mean'){
-        mean = value;
-        nd = new boost::normal_distribution<> (mean,std_dev);
-				var_nor = new boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > (rng, *nd);
-        return true;     
-      }
-      if (key == 'std_dev'){
-        std_dev = value;
-        nd = new boost::normal_distribution<> (mean,std_dev);
-				var_nor = new boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > (rng, *nd);
-        return true;   
-      }
-      return false;
-    }    
- 
-    
-    Pose run(Pose p){
-
-      Pose p_noise;
-
-      p_noise.x = p.x + (*var_nor)();
-      p_noise.y = p.y + (*var_nor)();
-      p_noise.z = p.z + (*var_nor)();
-      
-      return p_noise;
-    }
-
-  };
+    class AbstractNoise
+    {
+    public:
+        virtual Pose run (Pose p) = 0;
+        virtual bool setParam(std::string key, double value) = 0;
+    };
 
 
-	class NoiseGenerator{
-	private:
-	  AbstractNoise* pNoise;
-	 
-	public:
-	  NoiseGenerator (AbstractNoise& noise_type) : pNoise(&noise_type){}
+    class GaussianNoise : public AbstractNoise
+    {
+    private:
 
-	  void SetNoise(AbstractNoise& noise_type){
-	    pNoise = &noise_type;
-	  }
-	 
-	  Pose generateNoise(Pose p){
-	    return pNoise->run(p);
-	  }
-};
+        double mean;
+        double stddev;
+
+        boost::mt19937 rng;
+        boost::normal_distribution<> *normal;
+        boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > *generator;
+
+        void reset()
+        {
+            this->normal = new boost::normal_distribution(this->mean, this->stddev);
+            this->generator = new boost::variate_generator(this->rng, *(this->normal));
+        }
+
+    public:
+
+        GaussianNoise(double mean = 0.0, double stddev = 1.0)
+        {
+            this->mean = mean;
+            this->stddev = stddev;
+            this->reset();
+        }
+
+        bool setParam(std::string key, double value)
+        {
+            if (key == 'mean') { this->mean = value; this->reset(); return true; }
+            if (key == 'stddev') { this->stddev = value; this->reset(); return true; }
+            return false;
+        }
+
+        Pose run(Pose in)
+        {
+            Pose out;
+
+            out.x = in.x + (*generator)();
+            out.y = in.y + (*generator)();
+            out.z = in.z + (*generator)();
+
+            return out;
+        }
+
+    };
+
+
+    class NoiseGenerator
+    {
+    private:
+        AbstractNoise* pNoise;
+
+    public:
+
+        NoiseGenerator (AbstractNoise& noise_type)
+        {
+            this->setNoise(noise_type);
+        }
+
+        void setNoise(AbstractNoise& noise_type)
+        {
+            this->pNoise = &noise_type;
+        }
+
+        Pose generateNoise(Pose in)
+        {
+            return this->pNoise->run(in);
+        }
+    };
 }
